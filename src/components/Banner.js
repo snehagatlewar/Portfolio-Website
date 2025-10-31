77f371222d188612ccda5c16691fad89c576d7ba
Sneha Gatlewar <snehagatlewar64@gmail.com>
2025-10-31 13:09:25 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Software Developer", "FrontEnd Developer", "BackEnd developer", "Java FullStack Developer" ];
+  const toRotate = [ "Data Analyst", "Business Analyst" ];
    const period = 2000;
 
   useEffect (() => {
